be6b6b0afc600b201d5f4fc97ce48eacb758b13c
Abdul Rehman <arehman4275@gmail.com>
2025-11-11 19:13:15 +0500

    Refactor bot.h for improved structure and readability

--- bot.h
+++ bot.h
@@ -60,3 +60,87 @@ public:
 		return;
 	}
 };
+
+class HomeLoan {
+public:
+	int numberOfLines, area1Homes, area2Homes, area3Homes, area4Homes;
+	int* area, * size, * installments, * price, * downPayment;
+	HomeLoan() {
+		numberOfLines = numberOfLinesInFile("Home.txt") - 1;
+		area = new int[numberOfLines];
+		size = new int[numberOfLines];
+		installments = new int[numberOfLines];
+		price = new int[numberOfLines];
+		downPayment = new int[numberOfLines];
+		area1Homes = area2Homes = area3Homes = area4Homes = 0;
+	}
+	~HomeLoan() {
+		delete[] area;
+		delete[] size;
+		delete[] installments;
+		delete[] price;
+		delete[] downPayment;
+		area = size = installments = price = downPayment = nullptr;
+	}
+	void initializeHomeLoan() {
+		ifstream file("Home.txt");
+		string line;
+		getline(file, line); 
+
+		int index = 0;
+		while (getline(file, line) && index < numberOfLines) {
+			int pos = 0;
+			string token;
+
+			pos = line.find('#');
+			token = line.substr(0, pos);
+			area[index] = extractInteger(token); 
+			if (area[index] == 1) {
+				area1Homes++;
+			}
+			else if (area[index] == 2) {
+				area2Homes++;
+			}
+			else if (area[index] == 3) {
+				area3Homes++;
+			}
+			else if (area[index] == 4) {
+				area4Homes++;
+			}
+
+			line.erase(0, pos + 1);  
+
+			pos = line.find('#');
+			token = line.substr(0, pos);
+			size[index] = extractInteger(token);
+			line.erase(0, pos + 1);
+
+			pos = line.find('#');
+			token = line.substr(0, pos);
+			installments[index] = extractInteger(token);  
+			line.erase(0, pos + 1);
+
+			pos = line.find('#');
+			token = line.substr(0, pos);
+			price[index] = extractInteger(token); 
+			line.erase(0, pos + 1);
+
+			downPayment[index] = extractInteger(line); 
+
+			++index;
+		}
+	}
+	int extractInteger(string& str) {
+		int value = 0;
+		int start = 0;
+		while (start < str.length() && !isdigit(str[start])) {
+			start++;
+		}
+		for (int i = start; i < str.length(); i++) {
+			if (isdigit(str[i])) {
+				value = value * 10 + (str[i] - '0');
+			}
+		}
+		return value;
+	}
+};
