e7fe9024f44858c6eaad25020dcd48708a2bf593
Eren Yeager <erenyeagerabdulahad@gmail.com>
2025-11-15 18:57:54 +0500

    Added LoanSeeker Class for Loan Appliers

--- bot.h
+++ bot.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 
 int numberOfLinesInFile(string fileName) {
@@ -196,6 +198,225 @@ public:
 	}
 };
 
+class LoanSeeker {
+private:
+    bool isAlphabetOnly(const string& str) {
+        return all_of(str.begin(), str.end(), [](char c) { return isalpha(c) || isspace(c); });
+    }
+
+    bool isNumeric(const string& str) {
+        return all_of(str.begin(), str.end(), ::isdigit);
+    }
+
+    bool validateDate(const string& date) {
+        int day, month, year;
+        if (sscanf(date.c_str(), "%d %d %d", &day, &month, &year) == 3) {
+            if (month < 1 || month > 12) return false;
+            if (day < 1 || day > 31) return false;
+            if (month == 2) {
+                if (year % 4 == 0) {
+                    if (year % 100 == 0 && year % 400 != 0) return false;
+                    if (day > 29) return false;
+                } else {
+                    if (day > 28) return false;
+                }
+            }
+            if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) return false;
+            return true;
+        }
+        return false;
+    }
+
+    void capitalizeWords(string& str) {
+        bool newWord = true;
+        for (char& c : str) {
+            if (newWord && isalpha(c)) {
+                c = toupper(c);
+                newWord = false;
+            } else if (isspace(c)) {
+                newWord = true;
+            } else {
+                c = tolower(c);
+            }
+        }
+    }
+
+public:
+    string fullName, fatherName, postalAddress, contactNumber, email, cnic, cnicExpiryDate;
+    string employmentStatus, maritalStatus, gender;
+    int numberOfDependents;
+    float annualIncome, avgElectricityBill, currentElectricityBill;
+
+    void inputFullName() {
+        while (true) {
+            cout << "Enter Full Name: ";
+            getline(cin, fullName);
+            if (isAlphabetOnly(fullName)) {
+                capitalizeWords(fullName);
+                break;
+            } else {
+                cout << "Full Name should contain only alphabets and spaces! Please try again.\n";
+            }
+        }
+    }
+
+    void inputFatherName() {
+        while (true) {
+            cout << "Enter Father's Name: ";
+            getline(cin, fatherName);
+            if (isAlphabetOnly(fatherName)) {
+                capitalizeWords(fatherName);
+                break;
+            } else {
+                cout << "Father's Name should contain only alphabets and spaces! Please try again.\n";
+            }
+        }
+    }
+
+    void inputPostalAddress() {
+        cout << "Enter Postal Address: ";
+        getline(cin, postalAddress);
+        capitalizeWords(postalAddress);
+    }
+
+    void inputContactNumber() {
+        while (true) {
+            cout << "Enter Contact Number (11 or 13 digits): ";
+            getline(cin, contactNumber);
+            if (contactNumber.size() == 11 && contactNumber[0] == '0' || contactNumber.size() == 13 && contactNumber.substr(0, 3) == "+92") {
+                break;
+            } else {
+                cout << "Invalid contact number! Please try again.\n";
+            }
+        }
+    }
+
+    void inputEmail() {
+        while (true) {
+            cout << "Enter Email Address: ";
+            getline(cin, email);
+            transform(email.begin(), email.end(), email.begin(), ::tolower);
+            if (email.find('@') != string::npos) {
+                break;
+            } else {
+                cout << "Invalid email address! Please try again.\n";
+            }
+        }
+    }
+
+    void inputCnic() {
+        while (true) {
+            cout << "Enter CNIC (Without dashes): ";
+            getline(cin, cnic);
+            if (cnic.size() == 13 && all_of(cnic.begin(), cnic.end(), ::isdigit)) {
+                break;
+            } else {
+                cout << "Invalid CNIC! Please try again.\n";
+            }
+        }
+    }
+
+    void inputCnicExpiryDate() {
+        while (true) {
+            cout << "Enter CNIC Expiry Date (dd mm yy): ";
+            getline(cin, cnicExpiryDate);
+            if (validateDate(cnicExpiryDate)) {
+                break;
+            } else {
+                cout << "Invalid date format or values! Please enter in the format dd mm yy.\n";
+            }
+        }
+    }
+
+    void inputEmploymentStatus() {
+        while (true) {
+            cout << "Enter Employment Status (self, salaried, retired, unemployed): ";
+            getline(cin, employmentStatus);
+            transform(employmentStatus.begin(), employmentStatus.end(), employmentStatus.begin(), ::tolower);
+            if (employmentStatus == "self" || employmentStatus == "salaried" || employmentStatus == "retired" || employmentStatus == "unemployed") {
+                break;
+            } else {
+                cout << "Invalid employment status! Please try again.\n";
+            }
+        }
+    }
+
+    void inputMaritalStatus() {
+        while (true) {
+            cout << "Enter Marital Status (widowed, single, married, divorced): ";
+            getline(cin, maritalStatus);
+            transform(maritalStatus.begin(), maritalStatus.end(), maritalStatus.begin(), ::tolower);
+            if (maritalStatus == "widowed" || maritalStatus == "single" || maritalStatus == "married" || maritalStatus == "divorced") {
+                break;
+            } else {
+                cout << "Invalid marital status! Please try again.\n";
+            }
+        }
+    }
+
+    void inputGender() {
+        while (true) {
+            cout << "Enter Gender (male, female, other): ";
+            getline(cin, gender);
+            transform(gender.begin(), gender.end(), gender.begin(), ::tolower);
+            if (gender == "male" || gender == "female" || gender == "other") {
+                break;
+            } else {
+                cout << "Invalid gender! Please try again.\n";
+            }
+        }
+    }
+
+    void inputNumberOfDependents() {
+        cout << "Enter Number of Dependents: ";
+        string input;
+        getline(cin, input);
+        numberOfDependents = stoi(input);
+    }
+
+    void inputAnnualIncome() {
+        while (true) {
+            cout << "Enter Annual Income (no commas): ";
+            string input;
+            getline(cin, input);
+            if (isNumeric(input)) {
+                annualIncome = stof(input);
+                break;
+            } else {
+                cout << "Annual Income should contain only numbers! Please try again.\n";
+            }
+        }
+    }
+
+    void inputAvgElectricityBill() {
+        while (true) {
+            cout << "Enter Average Electricity Bill (no commas): ";
+            string input;
+            getline(cin, input);
+            if (isNumeric(input)) {
+                avgElectricityBill = stof(input);
+                break;
+            } else {
+                cout << "Average Electricity Bill should contain only numbers! Please try again.\n";
+            }
+        }
+    }
+
+    void inputCurrentElectricityBill() {
+        while (true) {
+            cout << "Enter Current Electricity Bill (exact): ";
+            string input;
+            getline(cin, input);
+            if (isNumeric(input)) {
+                currentElectricityBill = stof(input);
+                break;
+            } else {
+                cout << "Current Electricity Bill should contain only numbers! Please try again.\n";
+            }
+        }
+    }
+};
+
 void startBot() {
 	Responder responderForUtterances;
 	responderForUtterances.initializeResponder("Utterances.txt");
@@ -240,5 +461,6 @@ void startBot() {
 		}
 	}
 }
+
 
 
